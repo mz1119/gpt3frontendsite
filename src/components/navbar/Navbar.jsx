294cc4b1151ba83c767d9b2be7fc56e8d299b963
max <maxzhang1119@gmail.com>
2023-01-22 00:35:35 -0500

    v1 website done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Menu = () => (
   <>
     <p><a href="#home">Home</a></p>
     <p><a href="#wgpt3">What is GPT3?</a></p>
-    <p><a href="#possibilities">OpenAI</a></p>
+    <p><a href="#possibility">OpenAI</a></p>
     <p><a href="#features">Case Studies</a></p>
     <p><a href="#blog">Library</a></p>
   </>
